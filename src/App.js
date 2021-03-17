57e0cea3435b6999abf3c74cd5b7a2837cde10b7
= <tranminhtri9090@gmail.com>
2021-03-17 21:27:19 +1000

    Adding BlogSection for my portfolio

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Projects from './Components/ProjectSection/ProjectSection';
 import Aboutme from './Components/Aboutme/Aboutme';
+import Blog from './Components/Blogs/Blogs';
 import ContactMe from './Components/ContactMe/ContactMe';
 import Footer from './Components/Footer/Footer';
 
@@ -15,6 +16,8 @@ const Background = styled.div`
   background-size: cover;
   margin-top: -2em;
   font-family: 'Open Sans Condensed', sans-serif;
+  position: relative;
+  z-index: 1;
 
   @media (max-width: 870px) {
     margin-top: 0px;
@@ -28,6 +31,7 @@ function App() {
     <Home />
     <Projects /> 
     <Aboutme/>
+    <Blog /> 
     <ContactMe /> 
     <Footer />
   </Background>
